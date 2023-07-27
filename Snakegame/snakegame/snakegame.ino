08b991c07d67094b0e1082f5a4ceb5ad2b38c230
Paulo Macedo <paulomacedo@ua.pt>
2023-07-28 00:17:40 +0100

    Snake game working!

--- Snakegame/snakegame/snakegame.ino
+++ Snakegame/snakegame/snakegame.ino
@@ -2,20 +2,18 @@
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-const byte WIDTH = 16; // LCD width
-const byte HEIGHT = 2; // LCD height
+const byte WIDTH = 16; 
+const byte HEIGHT = 2; 
 
-// LCD has 32 matrices 5:8 
 const byte MATRIX_WIDTH = 5; 
 const byte MATRIX_HEIGHT = 8;
 
-// Snake properties
 const byte MAX_LENGTH = 28; // Maximum length of the snake
 byte snakeX[MAX_LENGTH][2]; // X's-coordinate of each segment of the snake
 byte snakeY[MAX_LENGTH][2]; // Y's-coordinate of each segment of the snake
-byte snakeLength = 1; // Initial length of the snake
+byte snakeLength = 1; 
 byte snakeDirection = 0; // Direction of the snake (0: right, 1: down, 2: left, 3: up)
-int snakeSpeed = 500;
+int snakeSpeed = 350;
 bool gameOver = false;
 bool gameWon = false;
 
@@ -33,19 +31,19 @@ coordinates food;
 matrix gameMatrix[2][16];
 
 void setup() {
+  Serial.begin(9600);
   lcd.begin(WIDTH, HEIGHT);
   lcd.clear();
-  //randomSeed(analogRead(A0)); // Seed the random number generator
   resetGame();
 }
 
 void loop() {
   if (!gameOver && !gameWon) {
-    handleInput();
+    snakeNextMove();
     moveSnake();
     checkCollision();
     drawGame();
-    delay(snakeSpeed); // Snake speed
+    delay(snakeSpeed); 
   }
 }
 
@@ -53,10 +51,8 @@ void resetGame() {
   snakeLength = 1;
   snakeDirection = 0;
 
-  // Initial snake X's coordinates 
   snakeX[0][0] = 0;
   snakeX[0][1] = 0;
-  // Initial snake Y's coordinates 
   snakeY[0][0] = 0;
   snakeY[0][1] = 0;
 
@@ -72,7 +68,6 @@ void generateFood() {
 }
 
 void resetGameMatrix() {
-  // Constant reset operation (256 op)
   for (int i = 0; i < 2; i++) {
     for (int j = 0; j < 16; j++) {
       for (int k = 0; k < 8; k++) {
@@ -92,14 +87,28 @@ void buildGameMatrices(){
   }
 }
 
-void handleInput() {
-  // Input handling code here
-  // snakeDirection = random(0, 4); Direction of the snake (0: right, 1: down, 2: left, 3: up)
-  snakeDirection = 0;
+void snakeNextMove() {
+  int distanceX = ((food.lcd_x * 5) + food.matrix_x) - ((snakeX[0][0] * 5) + snakeX[0][1]);
+  int distanceY = ((food.lcd_y * 8) + food.matrix_y) - ((snakeY[0][0] * 8) + snakeY[0][1]);
+
+  if (abs(distanceX) >= abs(distanceY)) {
+    if (distanceX > 0 && !willCollide(0)) {
+      snakeDirection = 0;
+    } else if (!willCollide(2)){
+      snakeDirection = 2;
+    }
+  } 
+
+  if(abs(distanceX) < abs(distanceY)){
+    if (distanceY > 0 && !willCollide(1)) {
+      snakeDirection = 1;
+    } else if (!willCollide(3)){
+      snakeDirection = 3;
+    }
+  } 
 }
 
 void moveSnake() {
-  // Move the snake
   for (int i = snakeLength - 1; i > 0; i--) {
     snakeX[i][0] = snakeX[i - 1][0];
     snakeX[i][1] = snakeX[i - 1][1];
@@ -222,4 +231,66 @@ void drawGame() {
     lcd.setCursor(0, 0);
     lcd.print("Game Won!");
   }
+}
+
+bool willCollide(int snakeDirection) {
+  byte nextLCDX = snakeX[0][0];
+  byte nextLCDY = snakeY[0][0];
+  byte nextMatrixX = snakeX[0][1];
+  byte nextMatrixY = snakeY[0][1];
+
+  switch (snakeDirection) {
+    case 0: // Right
+      if (nextLCDX >= 15 && nextMatrixX >= 4) { // x limit reached
+        nextLCDX  = 0;
+        nextMatrixX  = 0;
+      } else if (nextMatrixX  >= 4) { // x cell limit reached
+        nextLCDX  += 1;
+        nextMatrixX  = 0;
+      } else {
+        nextMatrixX  += 1;
+      }
+      break;
+    case 1: // Down
+      if (nextLCDY  >= 1 && nextMatrixY  >= 7) { // x limit reached
+        nextLCDY  = 0;
+        nextMatrixY  = 0;
+      } else if (nextMatrixY  >= 7) { // x cell limit reached
+        nextLCDY  += 1;
+        nextMatrixY  = 0;
+      } else {
+        nextMatrixY  += 1;
+      }
+      break;
+    case 2: // Left
+      if (nextLCDX  <= 0 && nextMatrixX  <= 0) { // x limit reached
+        nextLCDX  = 15;
+        nextMatrixX  = 4;
+      } else if (nextMatrixX <= 0) { // x cell limit reached
+        nextLCDX  -= 1;
+        nextMatrixX  = 4;
+      } else {
+        nextMatrixX  -= 1;
+      }
+      break;
+    case 3: // Up
+      if (nextLCDY  <= 0 && nextMatrixY  <= 0) { // x limit reached
+        nextLCDY  = 1;
+        nextMatrixY  = 7;
+      } else if (nextMatrixY  <= 0) { // x cell limit reached
+        nextLCDY  -= 1;
+        nextMatrixY  = 7;
+      } else {
+        nextMatrixY  -= 1;
+      }
+      break;
+  }
+
+  for (int i = 1; i < snakeLength && gameOver != true; i++) {
+    if (snakeX[i][0] == nextLCDX  && snakeX[i][1] == nextMatrixX  && snakeY[i][0] == nextLCDY  && snakeY[i][1] == nextMatrixY ) {
+      return true; 
+    }
+  }
+
+  return false; 
 }
